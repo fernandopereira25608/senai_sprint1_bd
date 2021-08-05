3187005e71455dec372ea2a98ca25337cbbd6b99
Fernando Santos Pereira <82387013+fernandopereira25608@users.noreply.github.com>
2021-08-04 21:33:33 -0300

    Add files via upload

---
+++ 1.0-exercicio-filmes/1.0_exercicio_filmes_DDL.sql.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE CATALOGO;
+GO
+
+USE CATALOGO;
+GO
+
+CREATE TABLE Genero(
+	IdGenero TINYINT PRIMARY KEY IDENTITY(1,1),
+	NomeGenero VARCHAR(30)
+);
+GO
+
+--(1,1) inicia com 1 e dps vai adicionando 1 a cada
+
+CREATE TABLE Filme(
+	IdFilme INT PRIMARY KEY IDENTITY(1,1),
+	IdGenero TINYINT FOREIGN KEY REFERENCES Genero(IdGenero),
+	TituloFilme VARCHAR(70)
+);
+GO
