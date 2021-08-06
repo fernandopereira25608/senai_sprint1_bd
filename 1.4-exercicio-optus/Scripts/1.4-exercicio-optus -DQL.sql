0cff8acf46f9ed7611180611d845005630b4efee
Fernando Santos Pereira <82387013+fernandopereira25608@users.noreply.github.com>
2021-08-05 21:33:37 -0300

    Add files via upload

---
+++ 1.4-exercicio-optus/Scripts/1.4-exercicio-optus -DQL.sql
@@ -0,0 +1,21 @@
+USE optus;
+GO
+
+SELECT * FROM Empresa;
+GO
+
+SELECT * FROM Usuario;
+GO
+
+SELECT * FROM Artista;
+GO
+
+SELECT * FROM Estilo;
+GO
+
+SELECT * FROM Album;
+GO
+
+SELECT * FROM EstiloAlbum;
+GO
+
